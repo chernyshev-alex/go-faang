35f7b2be28bd9beee90d0c5ded4c966feb532e38
chernyshev-alexander <achernyshev@griddynamics.com>
2022-06-25 16:13:20 +0200

    traversalBfsMatrix

--- graphs/graph.go
+++ graphs/graph.go
@@ -35,19 +35,14 @@ func traversal_dfs(v int, m [][]int, values *[]int, seen *map[int]bool) {
 // --- Adjacency Matrix ---
 
 func traversalBfsMatrix(m [][]int) []int {
-	seen, values := make(map[int]bool), make([]int, 0)
-	q := make([]int, 0)
+	seen, values, q := make(map[int]bool), make([]int, 0), make([]int, 0)
 	q = append(q, 0)
-
-	for len(q) > 0 {
-		var v int
-		v, q = q[0], q[1:]
-		values = append(values, v)
-		seen[v] = true
-
-		for connected, v := range m[v] {
-			if connected > 0 && !seen[v] {
-				q = append(q, v)
+	for row := 0; len(q) > 0; {
+		row, q = q[0], q[1:]
+		values, seen[row] = append(values, row), true
+		for col, connected := range m[row] {
+			if _, ok := seen[col]; !ok && connected > 0 {
+				q = append(q, col)
 			}
 		}
 	}
@@ -57,10 +52,8 @@ func traversalBfsMatrix(m [][]int) []int {
 func traversalDfsMatrix(v int, m [][]int, values *[]int, seen *map[int]bool) {
 	*values, (*seen)[v] = append(*values, v), true
 	for n, connected := range m[v] {
-		if connected > 0 {
-			if _, ok := (*seen)[n]; !ok {
-				traversalDfsMatrix(n, m, values, seen)
-			}
+		if _, ok := (*seen)[n]; !ok && connected > 0 {
+			traversalDfsMatrix(n, m, values, seen)
 		}
 	}
 }
